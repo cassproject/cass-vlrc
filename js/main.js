ba5d52e3db7fcc92bf648ed2fcc3a0baab9f129e
Ronald Ray <fritz.ray@eduworks.com>
2018-09-15 09:32:54 -0700

    Async + binding to app.

--- js/main.js
+++ js/main.js
@@ -1,83 +1,98 @@
 var repo = new EcRepository();
-repo.selectedServer = "https://dev.cassproject.org/api/"; 
+repo.selectedServer = "https://dev.cassproject.org/api/";
 EcRepository.caching = true;
-Vue.component('framework',{
-  props: ['uri'],
-  computed: {
-    competency:{
-      get:function(){
-        var r = {};
-        var top = {};
-        var f = EcFramework.getBlocking(this.uri);
-        if (f == null || f.competency == null) return r;
-        for (var i = 0;i < f.competency.length;i++)
-        {
-          var c = EcCompetency.getBlocking(f.competency[i]);
-          if (c != null)
-          r[c.shortId()]=top[c.shortId()]=c;
+Vue.component('framework', {
+    props: ['uri'],
+    data: function () {
+        return {
+            competency: null
         }
-        for (var i = 0;i < f.relation.length;i++)
-        {
-          var a = EcAlignment.getBlocking(f.relation[i]);
-          if (a != null)
-          {
-            if (a.relationType==Relation.NARROWS)
-            {
-              if (r[a.target] == null) continue;
-              if (r[a.target].hasChild == null)
-                r[a.target].hasChild = [];
-              r[a.target].hasChild.push(r[a.source]);
-              delete top[a.source];
+    },
+    computed: {
+        competencies: {
+            get: function () {
+                if (this.uri == null) return null;
+                if (this.competency != null)
+                    return this.competency;
+                var f = EcFramework.getBlocking(this.uri);
+                var precache = [];
+                if (f.competency != null) precache = precache.concat(f.competency);
+                if (f.relation != null) precache = precache.concat(f.relation);
+                var me = this;
+                repo.precache(precache, function (success) {
+                    var r = {};
+                    var top = {};
+                    if (f == null) return r;
+                    if (f.competency != null)
+                        for (var i = 0; i < f.competency.length; i++) {
+                            var c = EcCompetency.getBlocking(f.competency[i]);
+                            if (c != null)
+                                r[c.shortId()] = top[c.shortId()] = c;
+                        }
+                    if (f.relation != null)
+                        for (var i = 0; i < f.relation.length; i++) {
+                            var a = EcAlignment.getBlocking(f.relation[i]);
+                            if (a != null) {
+                                if (a.relationType == Relation.NARROWS) {
+                                    if (r[a.target] == null) continue;
+                                    if (r[a.target].hasChild == null)
+                                        r[a.target].hasChild = [];
+                                    r[a.target].hasChild.push(r[a.source]);
+                                    delete top[a.source];
+                                }
+                            }
+                        }
+                    me.competency = top;
+                    return top;
+                });
+                return null;
+            },
+            set: function (v) {
+                this.competency = v;
             }
-          }
+        },
+        name: function () {
+            if (this.uri == null)
+                return "N/A";
+            return EcFramework.getBlocking(this.uri).getName();
+        },
+        description: function () {
+            if (this.uri == null)
+                return null;
+            return EcFramework.getBlocking(this.uri).getDescription();
         }
-        console.log(top);
-        return top;
-      }
     },
-    name: function () {
-      if (this.uri == null)
-        return "N/A";
-      return EcFramework.getBlocking(this.uri).getName();
+    watch: {
+        uri: function (newUri, oldUri) {
+            this.competency = null;
+            console.log(this.uri);
+        }
     },
-    description: function() {
-      if (this.uri == null)
-        return null;
-      return EcFramework.getBlocking(this.uri).getDescription();
-    }
-  },
-  template: '<div>'+
-    '<div>{{ name }}<small v-if="description" class="block">{{ description }}</small></div>'+
-    '<ul><competency v-for="item in competency" v-bind:key="item.id" :uri="item.id" :hasChild="item.hasChild"></competency></ul></div>'
+    template: '<div>' +
+        '<div>{{ name }}<small v-if="description" class="block">{{ description }}</small></div>' +
+        '<ul v-if="competencies"><competency v-for="item in competencies" v-bind:key="item.id" :uri="item.id" :hasChild="item.hasChild"></competency></ul>' +
+        '<div v-else><br>Loading Framework...</div></div>'
 });
 
-Vue.component('competency',{
-  props: ['uri','hasChild'],
-  computed:{
-    name:{
-      get:function(){
-        if (this.uri == null) return null;
-        return EcCompetency.getBlocking(this.uri).getName();
-      }
+Vue.component('competency', {
+    props: ['uri', 'hasChild'],
+    computed: {
+        name: {
+            get: function () {
+                if (this.uri == null) return "Invalid Competency";
+                return EcCompetency.getBlocking(this.uri).getName();
+            }
+        },
+        description: {
+            get: function () {
+                if (this.uri == null) return "Could not resolve URI.";
+                return EcCompetency.getBlocking(this.uri).getDescription();
+            }
+        },
     },
-    description:{
-      get:function(){
-        if (this.uri == null) return null;
-        return EcCompetency.getBlocking(this.uri).getDescription();
-      }
-    },
-  },
-  template: '<li><span>{{ name }}</span><small v-if="description" class="block">{{ description }}</small>'+
-  '<ul><competency v-for="item in hasChild" v-bind:key="item.id" :uri="item.id" :hasChild="item.hasChild"></competency></ul></div>'
-+'</li>'
-});
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!',
-    login: false,
-    status: 'loading...',
-    selectedFramework: ''
-  }
+    template: '<li>' +
+        '<span>{{ name }}</span>' +
+        '<small v-if="description" class="block">{{ description }}</small>' +
+        '<ul><competency v-for="item in hasChild" v-bind:key="item.id" :uri="item.id" :hasChild="item.hasChild"></competency></ul></div>' +
+        '</li>'
 });
