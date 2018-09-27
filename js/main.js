5a4a79112089cc5dafb2cc12f71ddcf06d7fec91
Ronald Ray <fritz.ray@eduworks.com>
2018-09-27 14:10:32 -0700

    Edit your own areas.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,26 @@ var repo = new EcRepository();
 repo.selectedServer = "https://dev.cassproject.org/api/";
 EcRepository.caching = true;
 EcIdentityManager.readIdentities();
+
+$(document).ready(function () {
+
+    $("iframe").ready(function () {
+        $(window).on("message", function (event) {
+            if (event.originalEvent.data.message == "waiting") {
+                //Identity
+                $("iframe")[0].contentWindow.postMessage(JSON.stringify({
+                    action: "identity",
+                    identity: EcIdentityManager.ids[0].ppk.toPem()
+                }), window.location.origin);
+            };
+        });
+    });
+
+    $("#rad4").change(function (evt) {
+        if ($("#rad4:checked").length > 0)
+            $("#viewOutputFramework").attr("src", "cass-editor/index.html?server=" + repo.selectedServer + "&user=wait&origin=" + window.location.origin);
+        else
+            $("#viewOutputFramework").attr("src", "");
+    });
+
+});
