2bafaaef9aed3cbe7968bd98e38e6de1b34de856
Ronald Ray <fritz.ray@eduworks.com>
2019-02-06 17:41:18 -0800

    Paste from clipboard, style.

--- js/controls/jobPostingCreate.js
+++ js/controls/jobPostingCreate.js
@@ -4,24 +4,27 @@ Vue.component('jobPostingCreate', {
         return {
             jobPostingObject: null,
             candidateRequirement: "",
+            title: "New Position",
+            description: "",
             skills: [],
+            candidates: [],
         };
     },
     computed: {
         title: {
             get: function () {
-                return this.jobPostingObject.title;
+                return this.title;
             },
             set: function (newVal) {
-                this.jobPostingObject.title = newVal;
+                this.title = newVal;
             }
         },
         description: {
             get: function () {
-                return this.jobPostingObject.description;
+                return this.description;
             },
             set: function (newVal) {
-                this.jobPostingObject.description = newVal;
+                this.description = newVal;
             }
         },
         type: {
@@ -40,15 +43,24 @@ Vue.component('jobPostingCreate', {
         this.jobPostingObject.addOwner(EcPk.fromPem(app.me));
         var me = this;
         this.$nextTick(function () {
-            $("#tags").autocomplete({
+            $(".tags").autocomplete({
                 source: function (request, response) {
                     EcCompetency.search(repo, request.term, function (competencies) {
-                        for (var i = 0; i < competencies.length; i++) {
-                            competencies[i].label = competencies[i].getName();
-                            competencies[i].value = competencies[i].shortId();
-                        }
-                        response(competencies);
-                    }, console.error);
+                        var eah = new EcAsyncHelper();
+                        eah.each(competencies, function (competency, callback) {
+                            competency.label = competency.getName();
+                            competency.value = competency.shortId();
+                            EcFramework.search(repo, 'competency:"' + competency.getGuid() + '"',
+                                function (frameworks) {
+                                    if (frameworks.length > 0) competency.label += " (from " + frameworks[0].getName() + ")";
+                                    callback();
+                                }, callback);
+                        }, function (competencies) {
+                            response(competencies);
+                        });
+                    }, console.error, {
+                        size: 20
+                    });
                 },
                 select: function (event, ui) {
                     me.skills.push(ui.item.shortId());
@@ -60,9 +72,13 @@ Vue.component('jobPostingCreate', {
     methods: {
         saveNewPosting: function () {
             this.jobPostingObject.skills = this.skills;
+            this.jobPostingObject.description = this.description;
+            this.jobPostingObject.title = this.title;
             this.jobPostingObject.generateId(repo.selectedServer);
-            app.jobPostings.unshift(this.jobPostingObject);
-            EcRepository.save(this.jobPostingObject, function () {}, console.error);
+            var jobPosting = this.jobPostingObject;
+            EcRepository.save(this.jobPostingObject, function () {
+                app.jobPostings.unshift(jobPosting);
+            }, console.error);
             this.skills = [];
             this.jobPostingObject = new JobPosting();
             this.jobPostingObject.title = "New Position";
@@ -71,6 +87,96 @@ Vue.component('jobPostingCreate', {
         },
         removeRequirement: function (index) {
             this.skills.splice(index, 1);
+        },
+        removeCandidate: function (index) {
+            this.candidates.splice(index, 1);
+        },
+        readFromClipboard: function () {
+            var me = this;
+            navigator.clipboard.readText()
+                .then(text => {
+                    console.log('Pasted content: ', text);
+                    var lines = text.split(/\r?\n/);
+                    var first = true;
+                    var title = "";
+                    var description = "";
+                    me.candidates = [];
+                    for (var i = 0; i < lines.length; i++) {
+                        var line = lines[i].trim().replace(/	/g, "");
+                        if (line == "") continue;
+                        if (first) {
+                            me.title = line;
+                            first = false;
+                        } else {
+                            if (line.endsWith(":"))
+                            ;
+                            else if (line.startsWith("•") || line.startsWith("-") || line.startsWith("*"))
+                                me.candidates.push(line.substring(1));
+                            else
+                                description += "\n" + line;
+                        }
+                        description = description.trim();
+                    }
+                    me.description = description;
+                    app.$nextTick(function () {
+                        $(".autoSuggest").autocomplete({
+                            source: function (request, response) {
+                                EcRepository.unsigned = true;
+                                EcCompetency.search(repo, request.term, function (competencies) {
+                                    var eah = new EcAsyncHelper();
+                                    eah.each(competencies, function (competency, callback) {
+                                        competency.label = competency.getName();
+                                        competency.value = competency.shortId();
+                                        EcFramework.search(repo, 'competency:"' + competency.getGuid() + '"',
+                                            function (frameworks) {
+                                                if (frameworks.length > 0) competency.label += " (from " + frameworks[0].getName() + ")";
+                                                callback();
+                                            }, callback);
+                                    }, function (competencies) {
+                                        competencies.unshift({
+                                            label: " ---------- Add this as a new competency. ---------- ",
+                                            value: "ADD"
+                                        });
+                                        response(competencies);
+                                    });
+                                }, console.error, {
+                                    size: 20,
+                                    sort: JSON.stringify([{
+                                        "_score": "desc"
+                                    }])
+                                });
+                                EcRepository.unsigned = false;
+                            },
+                            select: function (event, ui) {
+                                var after = function () {
+                                    me.skills.push(ui.item.shortId());
+                                    var iam = $(event.target)[0];
+                                    $(".autoSuggest").each(function (index) {
+                                        if (this == iam)
+                                            me.candidates.splice(index, 1);
+                                    });
+                                    me.$nextTick(function () {
+                                        $(".autoSuggest").first().autocomplete('search');
+                                    });
+                                }
+                                if (ui.item.value == "ADD") {
+                                    var c = new EcCompetency();
+                                    c.addOwner(EcPk.fromPem(app.me));
+                                    c.name = $(event.target).val().trim();
+                                    c.generateId(repo.selectedServer);
+                                    ui.item = c;
+                                    EcRepository.save(c, after, console.error);
+                                    return;
+                                }
+                                after();
+                            }
+                        });
+                        $(".autoSuggest").first().autocomplete('search');
+                    });
+                })
+                .catch(err => {
+                    console.error('Failed to read clipboard contents: ', err);
+                });
         }
     },
     template: '<div class="jobPostingCreate"><hr><h3>New Posting</h3>' +
@@ -80,11 +186,12 @@ Vue.component('jobPostingCreate', {
         '<option value="jobPostingType://position">Position</option>' +
         '<option value="jobPostingType://temp">Temporary Position</option>' +
         '</select>' +
-        '<input type="text" placeholder="Position, Job or Gig Title." id="jobPostingCreateTitle" v-model="jobPostingObject.title"/>' +
-        '<textarea placeholder="Position, Job or Gig Description." id="jobPostingCreateDescription" v-model="jobPostingObject.description"/>' +
+        '<input type="text" placeholder="Position, Job or Gig Title." id="jobPostingCreateTitle" v-model="title"/>' +
+        '<textarea placeholder="Position, Job or Gig Description." id="jobPostingCreateDescription" v-model="description"/>' +
         '<h4 v-if="skills != null && skills.length > 0">An applicant for this job should be able to:</h4>' +
         '<ul><div v-for="(item,index) in skills" :key="item"><button v-on:click="removeRequirement(index);">Remove</button><competency :uri="item" :parentCompetent="true"/></div></ul>' +
-        '<label for="tags">Add Requirements: </label><input type="text" id="tags" v-model="candidateRequirement">' +
-        '<button v-on:click="saveNewPosting">Create New Posting</button>' +
+        '<ul><div v-for="(item,index) in candidates"><button v-on:click="removeCandidate(index);">Remove</button><input :value="item" class="autoSuggest" /></div></ul>' +
+        '<label for="tags">Add Requirements: </label><input type="text" class="tags" v-model="candidateRequirement">' +
+        '<button v-on:click="saveNewPosting">Create New Posting</button><button v-on:click="readFromClipboard">Read from Clipboard</button>' +
         '</div>'
 });
