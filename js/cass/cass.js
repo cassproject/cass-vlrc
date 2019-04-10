d06a2abd7deabe6ef9b1fed3c82fb3530968bca6
Ronald Ray <fritz.ray@eduworks.com>
2019-04-10 13:40:08 -0700

    Can add evidence to an evidence-empty assertion.

--- js/cass/cass.js
+++ js/cass/cass.js
@@ -70721,14 +70721,17 @@ EcAssertion = stjs.extend(EcAssertion, Assertion, [], function(constructor, prot
     };
     prototype.getEvidencesAsync = function(success, failure) {
         var results = new Array();
-        new EcAsyncHelper().each(this.evidence, function(e, callback0) {
-            e.decryptIntoStringAsync(function(str) {
-                results.push(str);
-                callback0();
-            }, callback0);
-        }, function(strings) {
+        if (this.evidence != null) 
+            new EcAsyncHelper().each(this.evidence, function(e, callback0) {
+                e.decryptIntoStringAsync(function(str) {
+                    results.push(str);
+                    callback0();
+                }, callback0);
+            }, function(strings) {
+                success(results);
+            });
+         else 
             success(results);
-        });
     };
     prototype.getEvidenceAsync = function(index, success, failure) {
         if (this.evidence[index] == null) {
